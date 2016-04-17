e880599e2e5b1ee523fe4d856faf54e97dd0a066
Nick Mohoric <nick.mohoric@gmail.com>
2016-04-17 11:01:28 -0400

    Split expand into its own file for clarity

--- src/Web/Bitly.hs
+++ src/Web/Bitly.hs
@@ -6,57 +6,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 
-module Bitly where
+module Web.Bitly where
 
-import           Control.Applicative
-import           Control.Monad.Trans.Either
-import Data.Aeson (parseJSON, FromJSON, ToJSON, (.:))
-import           Data.Aeson.Types
-import           Data.Proxy
-import           Data.Text (Text, pack)
-import GHC.Generics
-import Servant.API
+import Control.Monad.Trans.Either
+import Data.Proxy
 import Servant.Client
+import Web.Bitly.Expand
 
--- | Generic Types
-newtype HashText = HashText Text
-  deriving (Show, Eq, Ord, FromText, ToText, FromJSON)
-
-newtype UrlText = UrlText Text
-  deriving (Show, Eq, Ord, FromText, ToText, FromJSON)
-
-newtype Token = Token Text
-  deriving (Show, Eq, Ord, FromText, ToText)
-
-newtype Error = Error Text
-  deriving (Show, Eq, Ord, FromText, ToText, FromJSON)
-
--- | Expand Response
-data ExpandResponse = ExpandResponse {
-    expanded :: [Expand]
-  } deriving (Show, Eq, Ord, Generic)
-
-instance FromJSON ExpandResponse where
-    parseJSON (Object o) = do
-      d <- o .: "data"
-      ExpandResponse <$> d .: "expand"
-
--- | Expand
-data Expand = Expand {
-    global_hash :: Maybe HashText
-  , user_hash   :: Maybe HashText
-  , long_url    :: Maybe UrlText
-  , short_url   :: Maybe UrlText
-  , error       :: Maybe Error
-  } deriving (Show, Eq, Generic, Ord)
-
-instance FromJSON Expand
-
--- | Expand API
-type ExpandAPI = "v3" :> "expand" :> QueryParam "access_token" Token
-                          :> QueryParam "shortURL" UrlText 
-                          :> QueryParam "hash" HashText 
-                          :> Get '[JSON] ExpandResponse
 type API = ExpandAPI
 
 api :: Proxy API
